12709c8fd9e58e308bbb9232013bbfe790515478
Jacob Mischka <jacobmischka@gmail.com>
2015-05-23 12:55:40 -0500

    print counter

--- slee.py
+++ slee.py
@@ -5,10 +5,11 @@ num_cards = '100'
 r = requests.post('http://cf-vanguard.com/en/cardlist/cardsearch', data={'data[CardSearch][show_page_count]':num_cards, 'data[CardSearch][keyword]':'', 'cmd':'search'})
 r.encoding = 'utf-8'
 soup = BeautifulSoup(r.text)
+trs = soup.find(id="searchResult-table").find_all("tr")
 os.makedirs("cardfaces", exist_ok=True)
 with open("cards.json", "w") as file:
     cards = []
-    for tr in soup.find(id="searchResult-table").find_all("tr"):
+    for tr in trs:
         card = {}
         td = tr.find("td")
         for span in td.find_all("span"):
@@ -35,4 +36,5 @@ with open("cards.json", "w") as file:
                 pass
 
         cards.append(card)
+        print(str(len(cards))+" / "+str(len(trs)))
     json.dump(cards, file)
