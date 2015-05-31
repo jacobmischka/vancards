d352f76270da4c8f7d57af180b0cf21f86633167
Jacob Mischka <jacobmischka@gmail.com>
2015-05-30 23:57:48 -0500

    grabs clan, race, nation, trigger, and skill, and flavor text, text isn't poop anymore

--- update.py
+++ update.py
@@ -15,36 +15,62 @@ with open("cards.json", "w") as file:
         for span in td.find_all("span"):
             if span.get("class") == ["unit"]:
                 if span.string:
+                    if span.find("span"):
+                        string = span.find("span").string
+                    else:
+                        string = span.string
+                    words = str(string).split(": ")
+                    card[words[0]] = words[1]
+                else:
+                    if span.text == "[Nation]: ":
+                        src = span.find("img")["src"]
+                        card["[Nation]"] = src[src.find("co_")+3:src.rfind(".")]
+                    elif span.text == "[Skill Icon]: ":
+                        src = span.find("img")["src"]
+                        card["[Skill]"] = src[src.find("sk_")+3:src.rfind(".")]
+                    elif "[Trigger]" in span.text:
+                        src = span.find("img")["src"]
+                        card["Trigger"] = src[src.find("tr_")+3:src.rfind(".")]
+
+            elif span.string:
+                if "[Race]" in span.string or "[Clan]" in span.string:
                     words = str(span.string).split(": ")
                     card[words[0]] = words[1]
-            else:
-                if "[Name]" not in card:
+                elif "[Name]" not in card:
                     card["[Name]"] = span.string
                 elif "[Number]" not in card:
                     card["[Number]"] = span.string
+                elif "[Flavor Text]" not in card:
+                    text = span.decode(formatter=None)
+                    text = text[6:-7]
+                    card["[Flavor Text]"] = text
                 else:
-                    card["[Text]"] = span.string
+                    text = span.decode(formatter=None)
+                    text = text[6:-7]
+                    card["[Text]"] = text
 
         src = tr.find("th").find("img")['src']
-        if str(src).endswith(".jpg"):
-            filename = str(card["[Number]"]).replace("/", "-")+".jpg"
-            attempts = 3
-            while not os.path.isfile("cardfaces/"+filename):
-                try:
-                    data = urllib.request.urlopen("http://cf-vanguard.com/en/cardlist/"+str(src), None, 10).read()
-                    with open("cardfaces/"+filename, "wb") as img:
-                        img.write(data)
-                    card["[Image]"] = filename
-                except socket.timeout:
-                    attempts -= 1
-                    print("timeout (10s): http://cf-vanguard.com/en/cardlist/"+str(src))
-                    if attempts > 0:
-                        print("retrying...")
-                    else:
-                        break
-                except (urllib.error.HTTPError, urllib.error.URLError):
-                    print("error downloading: http://cf-vanguard.com/en/cardlist/"+str(src))
-                    break
+        filename = str(card["[Number]"]).replace("/", "-")+".jpg"
+        card["[Image]"] = filename
+        # if str(src).endswith(".jpg"):
+        #     filename = str(card["[Number]"]).replace("/", "-")+".jpg"
+        #     attempts = 3
+        #     while not os.path.isfile("cardfaces/"+filename):
+        #         try:
+        #             data = urllib.request.urlopen("http://cf-vanguard.com/en/cardlist/"+str(src), None, 10).read()
+        #             with open("cardfaces/"+filename, "wb") as img:
+        #                 img.write(data)
+        #             card["[Image]"] = filename
+        #         except socket.timeout:
+        #             attempts -= 1
+        #             print("timeout (10s): http://cf-vanguard.com/en/cardlist/"+str(src))
+        #             if attempts > 0:
+        #                 print("retrying...")
+        #             else:
+        #                 break
+        #         except (urllib.error.HTTPError, urllib.error.URLError):
+        #             print("error downloading: http://cf-vanguard.com/en/cardlist/"+str(src))
+        #             break
 
         cards.append(card)
         print(str(len(cards))+" / "+str(len(trs)))
