108a12f3cad56750ad262e73c03cd39a17080104
Alison Saia <littlestcake@gmail.com>
2015-05-30 22:01:29 -0400

    dumb positioning shit

--- src/game.lua
+++ src/game.lua
@@ -68,11 +68,14 @@ function game:init()
 	self.zones.p1.rearFrontRight:init(1075, 674, 182, 182, false)
 
 	self.zones.p1.deck = zone:new()
-	self.zones.p1.deck:init(100, 100, 150, 200, true, 50)
-    self.zones.p1.deck.addCard = function(self, card)
-        self.__index.addCard(self, card)
-        card:flip("down")
-    end
+	self.zones.p1.deck:init(1300, 800, 150, 200, true, 50)
+	self.zones.p1.deck.addCard = function(self, card)
+		self.__index.addCard(self, card)
+		card:flip("down")
+	end
+
+	self.zones.p1.damage = zone:new()
+	self.zones.p1.damage:init(431, 778, 209, 288, true)
 
 	local f = assert(io.open("cards.json", "r"))
 	local t = f:read("*all")
@@ -83,37 +86,40 @@ function game:init()
 		self.zones.p1.deck:addCard(card:new(cards[i]))
 	end
 
-    self.frame = loveframes.Create("frame")
-	self.frame:SetPos(canvasX(10), canvasY(350)):ShowCloseButton(false):SetSize(canvasX(400), canvasY(700))
+	self.frame = loveframes.Create("frame")
+	self.frame:SetPos(canvasX(0), canvasY(0)):ShowCloseButton(false):SetSize(canvasX(415), canvasY(1080))
+
+	--self.chat = loveframes.Create("frame")
+	--self.chat:SetPos(canvasX(1505), canvasY(0)):ShowCloseButton(false):SetSize(canvasX(415), canvasY(1080))
 
 	self.list = loveframes.Create("list", self.frame)
-	self.list:SetPos(canvasX(5), canvasY(40)):SetSize(canvasX(390), canvasY(650))
+	self.list:SetPos(canvasX(14), canvasY(49)):SetSize(canvasX(388), canvasY(1017))
 
 	-- self.name = loveframes.Create("text", self.frame)
 	-- self.name:SetText(""):SetY(30, true)
 
 	local numbers = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"}
 
-    self.image = loveframes.Create("image")
-    self.image:SetImage("res/sleeve_back.png")
-    self.list:AddItem(self.image)
+	self.image = loveframes.Create("image")
+	self.image:SetImage("res/sleeve_back.png")
+	self.list:AddItem(self.image)
 
 	self.powerForm = loveframes.Create("form")
 	self.powerForm:SetLayoutType("horizontal"):SetName("Power")
 	self.power = loveframes.Create("text")
-    self.power:SetText("0")
+	self.power:SetText("0")
 	self.powerForm:AddItem(self.power)
 	self.list:AddItem(self.powerForm)
 
 	self.shieldForm = loveframes.Create("form")
 	self.shieldForm:SetLayoutType("horizontal"):SetName("Shield")
 	self.shield = loveframes.Create("text")
-    self.shield:SetText("0")
+	self.shield:SetText("0")
 	self.shieldForm:AddItem(self.shield)
 	self.list:AddItem(self.shieldForm)
 
-    self.text = loveframes.Create("text")
-    self.list:AddItem(self.text)
+	self.text = loveframes.Create("text")
+	self.list:AddItem(self.text)
 
 end
 
@@ -122,7 +128,7 @@ function game:enter()
 end
 
 function game:resume()
-    loveframes.SetState("none")
+	loveframes.SetState("none")
 end
 
 function game:update(dt)
@@ -131,7 +137,7 @@ function game:update(dt)
 		self.card.y = mouseY() - self.card.dragging.dy
 	end
 
-    loveframes.update(dt)
+	loveframes.update(dt)
 end
 
 function game:draw()
@@ -165,60 +171,60 @@ function game:draw()
 	for i,zone in pairs(self.zones.p1) do
 		zone:draw()
 	end
-    if self.card then self.card:draw() end
+	if self.card then self.card:draw() end
 
 	-- Scale render target to screen
 	love.graphics.setCanvas()
 	love.graphics.draw(self.canvas, love.graphics.newQuad(0, 0, 1920, 1080, love.graphics.getWidth(), love.graphics.getHeight()))
 
-    loveframes.draw()
+	loveframes.draw()
 end
 
 function game:clickedCard(x, y)
-    local returncard
-    for i,zone in pairs(self.zones.p1) do
-        if zone:contains(x, y) then
-            for j,card in ipairs(zone.cards) do
-                if card:contains(x, y) then returncard = card end
-            end
-        end
-    end
-    return returncard
+	local returncard
+	for i,zone in pairs(self.zones.p1) do
+		if zone:contains(x, y) then
+			for j,card in ipairs(zone.cards) do
+				if card:contains(x, y) then returncard = card end
+			end
+		end
+	end
+	return returncard
 end
 
 function game:updateCardmenu(card)
-    self.frame:SetName(self.card.name)
-    self.image:SetImage(self.card.face)
+	self.frame:SetName(self.card.name)
+	self.image:SetImage(self.card.face)
 	self.power:SetText(self.card.power)
 	self.shield:SetText(self.card.shield)
-    self.text:SetText(self.card.text)
+	self.text:SetText(self.card.text)
 end
 
 function game:mousepressed(x, y, button)
 	loveframes.mousepressed(x, y, button)
 	x = (x/love.graphics.getWidth())*1920
-    y = (y/love.graphics.getHeight())*1080
-    self.card = nil
-    self.card = self:clickedCard(x, y)
+	y = (y/love.graphics.getHeight())*1080
+	self.card = nil
+	self.card = self:clickedCard(x, y)
 	if button == "l" then
 		if self.card then
-            if self.card and self.card.orientation == "up" then
-                self:updateCardmenu(self.card)
-            end
+			if self.card and self.card.orientation == "up" then
+				self:updateCardmenu(self.card)
+			end
 			self.card.dragging.active = true
 			self.card.dragging.dx = x - self.card.x
 			self.card.dragging.dy = y - self.card.y
 			self.card.dragging.x0 = self.card.x
 			self.card.dragging.y0 = self.card.y
 		end
-    end
+	end
 
 end
 
 function game:mousereleased(x, y, button)
 	loveframes.mousereleased(x, y, button)
-    -- x = (x/love.graphics.getWidth())*1920
-    -- y = (y/love.graphics.getHeight())*1080
+	-- x = (x/love.graphics.getWidth())*1920
+	-- y = (y/love.graphics.getHeight())*1080
 	if button == "l" and self.card then
 		for k,zone in pairs(self.zones.p1) do
 			if zone:contains(self.card.x, self.card.y) then
@@ -233,17 +239,17 @@ function game:mousereleased(x, y, button)
 			self.card.dragging.active = false
 		end
 	elseif button == "r" and self.card then
-        local card = self.card
-        if self.cardcontext then
-            self.cardcontext:Remove()
-            self.cardcontext = nil
-        end
-        self.cardcontext = loveframes.Create("menu")
-        self.cardcontext:AddOption("Rotate", false, function() card:rotate() end)
-        self.cardcontext:AddOption("Flip", false, function() card:flip() end)
-        self.cardcontext:SetPos(x, y)
-    end
-    self.card = nil
+		local card = self.card
+		if self.cardcontext then
+			self.cardcontext:Remove()
+			self.cardcontext = nil
+		end
+		self.cardcontext = loveframes.Create("menu")
+		self.cardcontext:AddOption("Rotate", false, function() card:rotate() end)
+		self.cardcontext:AddOption("Flip", false, function() card:flip() end)
+		self.cardcontext:SetPos(x, y)
+	end
+	self.card = nil
 end
 
 function mouseX()
@@ -255,23 +261,23 @@ function mouseY()
 end
 
 function canvasX(x)
-    return (x/1920) * love.graphics.getWidth()
+	return (x/1920) * love.graphics.getWidth()
 end
 
 function canvasY(y)
-    return(y/1080) * love.graphics.getHeight()
+	return(y/1080) * love.graphics.getHeight()
 end
 
 function game:keypressed(key, code)
-    loveframes.keypressed(key, code)
+	loveframes.keypressed(key, code)
 end
 
 function game:keyreleased(key)
-    loveframes.keyreleased(key)
+	loveframes.keyreleased(key)
 end
 
 function game:textinput(text)
-    loveframes.textinput(text)
+	loveframes.textinput(text)
 end
 
 return game
