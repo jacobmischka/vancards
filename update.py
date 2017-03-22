bae1e662d55489f88b56687d2aa3b1c78a3b12a4
Jacob Mischka <jacobmischka@gmail.com>
2017-03-21 19:29:47 -0500

    Fix update bugs
    Should be pretty tolerant of their crappy website now
    
    - [x] Text is written to FlavorText when FlavorText is empty but Text is not
    - [x] If there is anything other than (or in addition to) an image in Skill Icon, nothing is written to Skill or SkillIcon
    - [x] In some cases, SkillIcon is populated while Skill is not
    - [x] In some cases, SkillIcon is not an icon, but is Japanese
    - [x] If a value is -0 or -, it should be treated as blank

--- update.py
+++ update.py
@@ -10,79 +10,75 @@ def main():
 	r = requests.post('http://cf-vanguard.com/en/cardlist/cardsearch', data={'data[CardSearch][show_page_count]':num_cards, 'data[CardSearch][keyword]':'', 'cmd':'search'})
 	r.encoding = 'utf-8'
 	soup = BeautifulSoup(r.text, 'html.parser')
-	trs = soup.find(id="searchResult-table").find_all("tr")
-	os.makedirs("src/cardfaces", exist_ok=True)
+	trs = soup.find(id='searchResult-table').find_all('tr')
+	os.makedirs('src/cardfaces', exist_ok=True)
 	for tr in trs:
 		card = {}
-		td = tr.find("td")
-		for span in td.find_all("span"):
-			if span.get("class") == ["unit"]:
-				if span.string:
-					if span.find("span"):
-						string = span.find("span").string
+		td = tr.find('td')
+		a = td.find('a')
+		[name_span, number_span] = a.find_all('span')
+		card['[Name]'] = name_span.string
+		card['[Number]'] = number_span.string
+		spans = td.find_all('span')
+		for span in spans:
+			if span.get('class') == ['unit']:
+				attribute = ' '.join(span.stripped_strings)
+				unit = attribute.split('：') if '：' in attribute else attribute.split(':')
+				prop = unit[0].strip()
+				try:
+					if prop == '[Nation]':
+						src = span.find('img')['src']
+						card[prop] = src[src.find('co_') + 3 : src.rfind('.')]
+					elif prop == '[Skill Icon]':
+						try:
+							src = span.find('img')['src']
+							card['[Skill]'] = src[src.find('sk_') + 3 : src.rfind('.')]
+						except:
+							card['[Skill]'] = unit[1].strip()
+					elif prop == '[Trigger]':
+						src = span.find('img')['src']
+						card[prop] = src[src.find('tr_') + 3 : src.rfind('.')]
 					else:
-						string = span.string
-					words = str(string).split(": ")
-					if words[1] != "-":
-						card[words[0]] = words[1]
-				else:
-					if span.text == "[Nation]: ":
-						src = span.find("img")["src"]
-						card["[Nation]"] = src[src.find("co_")+3:src.rfind(".")]
-					elif span.text == "[Skill Icon]: ":
-						src = span.find("img")["src"]
-						card["[Skill]"] = src[src.find("sk_")+3:src.rfind(".")]
-					elif "[Trigger]" in span.text:
-						src = span.find("img")["src"]
-						card["[Trigger]"] = src[src.find("tr_")+3:src.rfind(".")]
+						value = unit[1].strip()
+						if value != '-' and value != '-0':
+							card[prop] = value
+				except:
+					card[prop] = ''
+		text_span = spans[-1]
+		text = ' '.join(text_span.stripped_strings)
+		if text != '-' and text != '-0':
+			card['[Text]'] = text
+		else:
+			card['[Text]'] = ''
 
-			else:
-				text = ' '.join(span.stripped_strings)
-				if "[Race]" in text or "[Clan]" in text:
-					words = str(text).split(": ")
-					if words[1] != "-":
-						card[words[0]] = words[1]
-				elif "[Name]" not in card:
-					card["[Name]"] = text
-				elif "[Number]" not in card:
-					card["[Number]"] = text
-				elif '[Illustrator]' not in card:
-					card['[Illustrator]'] = text
-				elif "[Flavor Text]" not in card:
-					if text != "-":
-						card["[Flavor Text]"] = text
-				else:
-					if text != "-":
-						card["[Text]"] = text
-
-		src = str(tr.find("th").find("img")['src'])
+		src = str(tr.find('th').find('img')['src'])
 		ext = os.path.splitext(src)[1]
 		if src:
-			filename = str(card["[Number]"]).replace("/", "-") + ext
+			filename = str(card['[Number]']).replace('/', '-') + ext
 			attempts = 3
-			card["[Image]"] = filename
-			while not os.path.isfile("src/cardfaces/" + filename):
+			card['[Image]'] = filename
+			while not os.path.isfile('src/cardfaces/' + filename):
 				try:
-					data = urllib.request.urlopen("http://cf-vanguard.com/en/cardlist/" + src, None, 10).read()
-					with open("src/cardfaces/"+filename, "wb") as img:
+					data = urllib.request.urlopen('http://cf-vanguard.com/en/cardlist/' + src, None, 10).read()
+					with open('src/cardfaces/'+filename, 'wb') as img:
 						img.write(data)
 				except socket.timeout:
 					attempts -= 1
-					print("timeout (10s): http://cf-vanguard.com/en/cardlist/" + src)
+					print('timeout (10s): http://cf-vanguard.com/en/cardlist/' + src)
 					if attempts > 0:
-						print("retrying...")
+						print('retrying...')
 					else:
 						break
 				except (urllib.error.HTTPError, urllib.error.URLError):
-					print("error downloading: http://cf-vanguard.com/en/cardlist/" + src)
+					print('error downloading: http://cf-vanguard.com/en/cardlist/' + src)
 					break
 
 		cards.append(card)
-		print(str(len(cards))+" / "+str(len(trs)))
+		print(str(len(cards)) + ' / ' + str(len(trs)))
 
 
 def save_cards():
-	with open("cards.json", "w") as file:
+	with open('cards.json', 'w') as file:
 		json.dump(cards, file, indent=1)
 
 if __name__ == '__main__':
